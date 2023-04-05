2122b903665a997d808bbdd2dae015963f1c072f
idgrishin <kolyanchik2289@yandex.ru>
2023-04-05 14:40:48 +0300

    HUE2-3/added code quality tools

---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    parserPreset: {
+        parserOpts: {
+            issuePrefixes: ['HUE2-'],
+        },
+    },
+    rules: {
+        'header-max-length': [6, 'always', 120],
+        'references-empty': [2, 'never'],
+    },
+}
