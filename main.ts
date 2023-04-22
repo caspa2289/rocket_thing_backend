34573db832cbc2b74bd8dbe07c4b8daad3d8774d
idgrishin <kolyanchik2289@yandex.ru>
2023-04-22 17:01:34 +0300

    HUE2-20/ws auth test

--- main.ts
+++ main.ts
@@ -4,16 +4,31 @@ import { userApi } from './src/routes/user'
 import { setupMiddlewares } from './src/middlewares'
 import { notificationApi } from './src/routes/notification'
 import * as https from 'https'
+import http from 'http'
+import cors from 'cors'
+import bodyParser from 'body-parser'
 
 dotenv.config()
 
 async function startServer() {
     const app = express()
 
-    const serverPort = Number(process.env.PORT) || 6969
-    const server = https.createServer(app)
+    const clientPort = Number(process.env.CLIENT_PORT) || 3000
 
-    setupMiddlewares(process.env, app, server)
+    const corsOptions = {
+        credentials: true,
+        origin: [
+            `http://127.0.0.1:${clientPort}`,
+            `http://localhost:${clientPort}`,
+            'http://localhost:63342',
+            'https://caspa2289.github.io',
+        ],
+    }
+
+    app.use(cors(corsOptions))
+    app.use(bodyParser.json())
+
+    const serverPort = Number(process.env.PORT) || 6969
 
     app.use('/', userApi)
     app.use('/', notificationApi)
@@ -21,6 +36,10 @@ async function startServer() {
     app.get('/', (_, res) => {
         res.send('It`s alive!')
     })
+
+    const server = https.createServer(app)
+
+    setupMiddlewares(process.env, app, server)
 
     server.listen(serverPort, () => {
         console.log(`\x1b[32mSERVER LISTENING ON PORT ${serverPort}`)
--- src/middlewares/websocket.ts
+++ src/middlewares/websocket.ts
@@ -1,5 +1,6 @@
 import { Express, RequestHandler } from 'express'
 import { Server as HTTPSServer } from 'https'
+import { Server as HTTPServer } from 'http'
 import { Server } from 'socket.io'
 const sharedsession = require('express-socket.io-session')
 
@@ -7,7 +8,7 @@ export const setupWebsocketMiddleware = (
     session: RequestHandler,
     env: Record<string, any>,
     app: Express,
-    server: HTTPSServer
+    server: HTTPSServer | HTTPServer
 ) => {
     const io = new Server({
         cors: {
@@ -17,6 +18,7 @@ export const setupWebsocketMiddleware = (
                 'http://localhost:63342',
                 'https://caspa2289.github.io',
             ],
+            credentials: true,
         },
     })
 
